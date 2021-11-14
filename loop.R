6bf70c3ea6860d4f5227a75eeaac65e1728b2454
Nicolas Bressoud <nicolas.bressoud@hepvs.ch>
2021-11-14 13:02:54 +0100

    OK ! C'est prêt, avec deux params.

--- loop.R
+++ loop.R
@@ -1,15 +1,27 @@
+# Source : https://bookdown.org/yihui/rmarkdown-cookbook/parameterized-reports.html
+
 library(fivethirtyeight)
 library(tidyverse)
 library(rmarkdown)
 
-state <- bad_drivers$state
+# gérer le répertoire de destination (le créer au besoin)
+dir.create(path = "output",
+           showWarnings = FALSE)
+
+# préparer la liste des id et l'identification de l'année
+
+id <- bad_drivers$state
+year <- 2021
+
+# création du tibble pour générer les rapports
 
 reports <- tibble(
   input = "report.Rmd",
-  output_file = stringr::str_c("output/", state, "-driving.html"),
-  params = map(state, ~list(state = .))
+  output_file = str_c("output/", id, "-driving-", format(Sys.time(), "%Y%m%d%l%M"), ".html"),
+  params = map(id, ~list(id = . , year = year))
 )
+
+# génération du rapport en boucle avec pwalk.
 
 reports %>%
   pwalk(render)
-
